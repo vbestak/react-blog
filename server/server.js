3f45090c3a1c57bb58c719ba94f47e6ccb70834f
Valentino Bestak <valentino.bestak@gmail.com>
2020-09-01 20:01:11 +0200

    cors

--- server/server.js
+++ server/server.js
@@ -6,5 +6,17 @@ const config = require('./config');
 const port = config.port;
 
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 
 app.listen(port);
