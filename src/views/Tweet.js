a64cfec4de4038258e62b82e48de32fdddb822ed
ValentinoBestak <valentino.bestak@gmail.com>
2020-09-07 22:14:40 +0200

    added tweet view

---
+++ src/views/Tweet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tweet from '../components/Tweet'
+
+function Tweet() {
+    return (
+        <div>
+            <p> Post a new status (tweet) on your profile page</p>
+            <Tweet />
+        </div>
+
+)
+}
+
+export default Tweet
