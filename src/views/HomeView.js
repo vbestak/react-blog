8f571e9a07a1cff92177b953b289be2351958cec
ValentinoBestak <valentino.bestak@gmail.com>
2020-09-26 20:57:29 +0200

    style edit at home page

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomeView() {
     return (
-        <div>
+        <div style={{ minHeight: "80vh",textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center"}}>
             <p>Hello please login to your twitter account!</p>
             <a href="http://localhost:8085/login">Login</a>
         </div>
